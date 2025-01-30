edd075ca840166876217430d94d9c907dd9c3022
Nathaniel M Coleman <kyloren@turtleshelldesigns.com>
2025-01-30 16:51:13 -0500

    refactor with a separate function to get the highest bid

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,28 +1,31 @@
 from art import logo
 
+
+def find_highest_bid(dictionary):
+    winner = ""
+    highest_bid = 0
+
+    for user in dictionary:
+        amount = dictionary[user]
+        if amount > highest_bid:
+            highest_bid = amount
+            winner = user
+
+    print(f"The winner is {winner} with bid of ${highest_bid}")
+
+
 print(logo)
 bid_data = {}
-
 auction_will_continue = True
 
 while auction_will_continue:
-  # TODO-1: Ask the user for input
-  name = input("What is your name?: ")
-  bid = int(input("What is your bid?: $"))
-  continue_auction = input("Are there any other bidders? Type 'yes' or 'no': ")
-  # TODO-2: Save data into dictionary {name: price}
-  bid_data[name] = bid
-  # TODO-3: Whether if new bids need to be added
-  if continue_auction == 'no':
-    auction_will_continue = False
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    continue_auction = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    bid_data[name] = bid
 
-  print("\n" * 25)
-# TODO-4: Compare bids in dictionary
-max_key = max(bid_data, key=bid_data.get)
-# print(bid_data)
-print(f"The winner is {max_key} with bid of ${bid_data[max_key]}")
-
-# my_dict = {'a': 10, 'b': 25, 'c': 1005, 'Jasper': 2000}
-#
-# max_key = max(my_dict, key=my_dict.get)
-# print(max_key)
+    if continue_auction == 'no':
+        auction_will_continue = False
+        find_highest_bid(bid_data)
+    elif continue_auction == 'yes':
+        print("\n" * 20)
